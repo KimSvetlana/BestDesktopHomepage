d3043f9612d4e80fdb456246a8ccb81b241dd661
svetlana.kim <svetlana.kim.7008@gmail.com>
2021-04-07 16:43:39 +0700

    компонент новостей

--- src/components/main.vue
+++ src/components/main.vue
@@ -5,20 +5,19 @@
     <AboutComp/>
     <WorksComp/>
     <PriceComp/>
-    <FreeTrialComp/>
-    <SubscribeComp/>
-    <Footer/>
-    <!--
     <section class='comments'>
       <div class='row'>
         <div class="col img"></div>
         <div class="col carousel">
           <h2>Trusted By More Than 55M Peoples</h2>
           <CommentCard />
-
         </div>
       </div>
-    </section>-->
+    </section>
+    <NewsComp/>
+    <FreeTrialComp/>
+    <SubscribeComp/>
+    <Footer/>
 
   </div>
 </template>
@@ -30,6 +29,7 @@ import FeaturesComp from './features.vue'
 import AboutComp from './aboutComp.vue'
 import WorksComp from './worksComp.vue'
 import PriceComp from './priceComp.vue'
+import NewsComp from './newsComp.vue'
 import FreeTrialComp from './freeTrialComp.vue'
 import SubscribeComp from './subscribeComp.vue'
 import Footer from './footer.vue'
@@ -43,23 +43,25 @@ import CommentCard from './commentCard.vue'
     AboutComp,
     WorksComp,
     PriceComp,
-    SubscribeComp,
+    NewsComp,
     FreeTrialComp,
+    SubscribeComp,
     Footer,
     CommentCard
     // Carousel
   }
 
 })
-export default class Main extends Vue {
-  // @Prop() private msg!: string;
-}
+export default class Main extends Vue {}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
    @import '../assets/variables.scss';
 
+  section.comments{
+    background: $lightBackgroundColor;
+  }
   section.comments .img{
     height: 445px;
     background: url('../assets/images/Image.png') no-repeat;
---
+++ src/components/newsComp.vue
@@ -0,0 +1,75 @@
+<template>
+    <section class='container news-comp'>
+        <h2 class='bannerH2'>Latest News From Us</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore<br>
+         et dolore magna aliqua. Lorem ipsum dolor sit amet.</p>
+        <div class='row'>
+            <div class="col text-start">
+                <img src='../assets/images/image6.png'/>
+                <h3 class="news-title">The master bedroom suite is phenomenally spacious</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a href="" class='news-link'>Continue Reading <img src="../assets/images/ArrowLeft2.svg" alt=""></a>
+            </div>
+            <div class="col">
+                <div class='news-card'>
+                    <img src="../assets/images/image9.png" alt="">
+                    <h3 class='new-card-title'>The master bedroom suite is phenomeny spacious</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                </div>
+                <div class='news-card' >
+                    <img src="../assets/images/image7.png" alt="">
+                    <h3 class='new-card-title'>The master bedroom suite is phenomeny spacious</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                </div>
+                <div class='news-card'>
+                    <img src="../assets/images/image8.png" alt="">
+                    <h3 class='new-card-title'>The master bedroom suite is phenomeny spacious</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p></div>
+            </div>
+
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {}
+
+})
+export default class NewsComp extends Vue {}
+</script>
+
+<style lang="scss">
+    @import '../assets/variables.scss';
+    section.news-comp{
+        padding-top: 130px;
+    }
+    .news-comp h2{
+        font-weight: bold;
+    }
+    .news-link{
+        color:$darkBackgroundColor;
+        text-decoration: none;
+        font-weight: bold;
+        &:hover{
+            text-decoration: underline;
+        }
+    }
+    .new-card-title{
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 27px;
+    }
+    .news-card{
+        position: relative;
+        padding-left: 200px;
+
+        & img{
+            position: absolute;
+            top:0;
+            left:0;
+        }
+    }
+</style>
